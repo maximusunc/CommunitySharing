a850ad9d9929df3f5a95837226764f98e0723687
Matthew Nagy <grndprxguy@gmail.com>
2017-12-10 11:28:46 -0500

    psuedo coded routes

--- app/routes/share-api-routes.js
+++ app/routes/share-api-routes.js
@@ -1,1 +1,11 @@
 // api routes for shared items
+
+// get - /api/share/:itemId - show shares for an item
+
+// get - /api/share/:userId - show shares for a user
+
+// post - /api/share - create a new share between user and item
+
+// update - /api/share/:id - update a share
+
+// delete - /api/share/:id - delete a share
