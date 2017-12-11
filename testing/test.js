e738bec229dfc747ac03ce4bbde8f1676f3e0b45
Max Wang <maxwellswang@gmail.com>
2017-12-11 14:40:53 -0500

    testing

---
+++ testing/test.js
@@ -0,0 +1,64 @@
+var expect = require("chai").expect;
+var Nightmare = require("nightmare");
+var nightmare = Nightmare({ show: true });
+
+describe("Routes", function() {
+    it("should go to the login page", function(done) {
+        nightmare
+        .goto("localhost:8080/maxisawesome")
+        .evaluate(function() {
+            return document.title;
+        })
+        .then(function(result) {
+            expect(result).to.equal("Stuffshare");
+            done();
+        });
+    });
+    it("should login to the user page", function(done) {
+        nightmare
+        .goto("localhost:8080")
+        .type("#username", "John Doe")
+        .type("#email", "test@123.com")
+        .click("#login")
+        .evaluate(function() {
+            return document.querySelector("h1");
+        })
+        .then(function(result) {
+            expect(result).to.equal("John Doe");
+            done();
+        });
+    });
+    it("should view items of a user", function(done) {
+        nightmare
+        .goto("localhost:8080")
+        .type("#username", "John Doe")
+        .type("#email", "test@123.com")
+        .click("#login")
+        .click("#viewItems")
+        .evaluate(function() {
+            return document.querySelector("h1");
+        })
+        .then(function(result) {
+            expect(result).to.equal("Items");
+            done();
+        });
+    });
+    it("should view available items for borrow", function(done) {
+        nightmare
+        .goto("localhost:8080")
+        .type("#username", "John Doe")
+        .type("#email", "test@123.com")
+        .click("#login")
+        .click("#categories")
+        .click("#chainsaw")
+        .evaluate(function() {
+            return document.querySelector("h1");
+        })
+        .then(function(result) {
+            expect(result).to.equal("Chainsaw");
+            done();
+        });
+    });
+});
+
+describe("")
