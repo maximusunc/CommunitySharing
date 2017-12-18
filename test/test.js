0b7af1b93721af83b493b7759318e3954ad4cdac
Max Wang <maxwellswang@gmail.com>
2017-12-18 16:16:15 -0500

    continuing work on nightmare tests

--- test/test.js
+++ test/test.js
@@ -38,15 +38,12 @@ describe("Routes", function() {
             console.log("Search 2 failed: " + error);
         });
     });
-    it("should view available items for borrow", function() {
+    it("should fail to login", function() {
         new Nightmare({show:true})
         .goto("http://localhost:8080")
-        .type("#email", "rumple@rumple.com")
-        .type("#password", "test")
+        .type("#email", "notgoing@towork.com")
+        .type("#password", "wrongpassword")
         .click("#login")
-        .wait(2000)
-        .select([value="Tool"])
-        .click("borrowItem")
         .wait(2000)
         .evaluate(function() {
             return document.querySelector("h1").innerText;
@@ -54,10 +51,34 @@ describe("Routes", function() {
         .end()
         .then(function(result) {
             console.log(result);
-            expect(result).to.equal("Rumple");
+            expect(result).to.equal("Welcome To StuffShare!");
         })
         .catch(function(error) {
             console.log("Search 3 failed: " + error);
+        });
+    });
+    it("should view available items for borrow", function() {
+        new Nightmare({show:true})
+        .goto("http://localhost:8080")
+        .type("#email", "rumple@rumple.com")
+        .type("#password", "test")
+        .click("#login")
+        .wait(2000)
+        .click("select[name=category]")
+        .wait(4000)
+        // .select("#borrow", "Tool")
+        // .click("borrowItem")
+        // .wait(2000)
+        .evaluate(function() {
+            return document.querySelector("option").innerText;
+        })
+        .end()
+        .then(function(result) {
+            console.log(result);
+            expect(result).to.equal("Tool");
+        })
+        .catch(function(error) {
+            console.log("Search 4 failed: " + error);
         });
     });
 });
