6c14342ac73d1ada90bc85933d760195dca4c139
Matthew Nagy <grndprxguy@gmail.com>
2017-12-19 21:01:15 -0500

    removed date fields from shares

--- models/share.js
+++ models/share.js
@@ -1,19 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
 	var Share = sequelize.define("Share", {
-		share_date: {
-		        type: DataTypes.DATE,
-		        defaultValue: sequelize.NOW,
-		        allowNull: false
-		    },
-		    promise_date: {
-		    	type: DataTypes.DATEONLY,
-		    	allowNull: true
-		    },
-			return_date: {
-			type: DataTypes.DATE,			
-			allowNull: true
-			}
-
 	});
 
 	Share.associate = function(models) {
